a01971319615e966e2a235788b7d9ebd7b31b612
Evgenya888 <e.v.a240967@gmail.com>
2024-09-19 13:09:44 +0700

    Add files via upload

---
+++ pythonProject4/module_1_3.py
@@ -0,0 +1,8 @@
+name = 'Evgenya'
+print(name, type(name))
+age = 56
+print(age, type(age))
+new_age = 15
+print(age + new_age, type(age + new_age))
+is_student = 'true'
+print(is_student, type(is_student))
